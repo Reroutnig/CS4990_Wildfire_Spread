ceb84c6fb8efa285e35111bf9533b108f82e175a
uuriah <132951774+uuriah@users.noreply.github.com>
2025-05-06 14:34:08 -0700

    wv temsors

--- viewTif.py
+++ viewTif.py
@@ -1,4 +1,4 @@
 import rasterio
 from rasterio.plot import show
-tiff_img = rasterio.open('data/2025-04-13-a6ee51/ndvi_tifs/MOD13C2.A2020001.061.2020328142931_NDVI_US.tif')
+tiff_img = rasterio.open('data/burn_usa_tifs/burn_2024_4.tif')
 show(tiff_img)
